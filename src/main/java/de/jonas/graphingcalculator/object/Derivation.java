fbe09b1e5f9dfbb90ced8900e79628ef06f6d725
Jonas Lobe <jonas.lobe.2006@outlook.com>
2023-12-27 21:15:06 +0100

    simplify code

--- src/main/java/de/jonas/graphingcalculator/object/Derivation.java
+++ src/main/java/de/jonas/graphingcalculator/object/Derivation.java
@@ -11,17 +11,16 @@ import java.util.NavigableMap;
  * Eine {@link Derivation Ableitung} besteht aus einigen Funktionswerten der Ableitung und einem Zustand, ob sie
  * gezeichnet werden soll oder nicht.
  */
+@Getter
 @NotNull
 @RequiredArgsConstructor
 public final class Derivation {
 
     //<editor-fold desc="LOCAL FIELDS">
     /** Die Funktionswerte dieser Ableitung. */
-    @Getter
     @NotNull
     private final NavigableMap<Double, Double> derivationValues;
     /** Der Zustand, ob diese Ableitung angezeigt werden soll oder nicht. */
-    @Getter
     @Setter
     private boolean draw;
     //</editor-fold>
