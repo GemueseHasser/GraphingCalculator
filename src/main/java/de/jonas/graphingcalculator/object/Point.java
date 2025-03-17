dd6197d2e7f7aa284ff93f257f720e4ed1a26ae7
GemueseHasser <jonas.lobe.2006@outlook.com>
2025-03-17 08:10:49 +0100

    use lombok Getter for point

--- src/main/java/de/jonas/graphingcalculator/object/Point.java
+++ src/main/java/de/jonas/graphingcalculator/object/Point.java
@@ -7,16 +7,15 @@ import org.jetbrains.annotations.NotNull;
 /**
  * Ein {@link Point Punkt} besteht aus einer x- und einer y-Koordinate, wodurch dieser Punkt definiert wird.
  */
+@Getter
 @NotNull
 @RequiredArgsConstructor
 public final class Point {
 
     //<editor-fold desc="LOCAL FIELDS">
     /** Die x-Koordinate dieses Punkts. */
-    @Getter
     private final double x;
     /** Die y-Koordinate dieses Punkts. */
-    @Getter
     private final double y;
     //</editor-fold>
 
